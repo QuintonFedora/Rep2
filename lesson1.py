24822f25358f08bef72a602973d52c7a4500931f
fedoraq_wlhs <fedoraq@wlhs.wlwv.k12.or.us>
2024-09-24 09:49:38 -0700

    New file made

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+'''
+Name: Quinton Fedora
+Date: 9/16/24
+Assignment: Unit 1 Lesson 1 Notes
+'''
+
+
+# Variables
+
+message = "Hello, User"
+print(message)
+
+# snake_case
+user_name = input("Enter your name: ")
+user_id = int(input("Enter your id: "))
+
+# variable type
+# print(type(user_name))
+
+# strings
+# can use ' or " to indicate strings - be consistent
+
+# f-strings are formatted strings that help with combining strings
+# way 1 to combine string: use +
+message_one = "Welcome " + user_name + " with ID " + str(user_id)
+print(message_one)
