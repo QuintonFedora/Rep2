2342b95267a5395f02d119882de85423cb7b91fa
fedoraq_wlhs <fedoraq@wlhs.wlwv.k12.or.us>
2024-10-01 09:21:51 -0700

    unit2HW1 work

--- Unit2HW1.py
+++ Unit2HW1.py
@@ -36,3 +36,17 @@ ASCII_ART = r'''
 print(ASCII_ART)
 
 print('Problem 3'.center(20,'-'))
+
+#173.8 mi from portland to seattle
+
+distance_to_seattle = 173.8
+miles_per_gallon = int(input("Enter your car's fuel efficiency in miles per gallon (whole number): "))
+gas_price_per_gallon = float(input("Enter the price of gas per gallon: "))
+tank_capacity = int(input("Enter the capacity of your gas tank in gallons: "))
+gallons_needed = distance_to_seattle / miles_per_gallon
+if gallons_needed > tank_capacity:
+    print("Warning: You will need to refuel on your trip!")
+total_cost = tank_capacity * gas_price_per_gallon
+print(f"To drive from Portland to Seattle ({distance_to_seattle} miles):")
+print(f"Gallons needed: {gallons_needed:}")
+print(f"Total cost to fill your gas tank: ${total_cost:}")
