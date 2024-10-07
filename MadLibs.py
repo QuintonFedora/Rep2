974c387e20e3415a68d7b96de777f371eec346bd
fedoraq_wlhs <fedoraq@wlhs.wlwv.k12.or.us>
2024-10-07 09:12:07 -0700

    Madlibs work

---
+++ MadLibs.py
@@ -0,0 +1,23 @@
+#Create a Story Template
+#Design a story with placeholders for different types of words.
+#For example:
+#"Once upon a time, there was a [noun] who loved to [verb] with their [adjective] [noun]."
+
+#Gather User Input
+#Your program should ask the user to input the following:
+#A person's name (make sure the name is formatted in title case).
+#A month, day, and year (for a specific date).
+#At least 5 nouns, 5 verbs, 3 adjectives, and 2 adverbs.
+
+#Replace Placeholders
+# #Use the words provided by the user to replace the placeholders in your story template.
+# This should be done using f-strings to format the output.
+#Display the Final Story
+#After gathering all the inputs, display their name,
+#followed by a new line, the date (mm/dd/yy), followed by a new line,
+#then the completed story to the user with the words they provided
+
+
+User_name = input("What is your name? (capitalized)")
+What_date = input('What is a Month, Day , and Year? (mm/dd/yy)')
+Nouns = input('')
