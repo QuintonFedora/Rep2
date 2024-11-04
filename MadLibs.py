620efb1fa3159901eb770dd5c5968028ad12bed2
QUINTON FEDORA <fedoraq@wlhs.wlwv.k12.or.us>
2024-11-04 12:23:45 -0800

    MadLibsproject.py

--- MadLibs.py
+++ MadLibs.py
@@ -1,20 +1,6 @@
-#Create a Story Template
-#Design a story with placeholders for different types of words. For example:
-#"Once upon a time, there was a [noun] who loved to [verb] with their [adjective] [noun]."
-#Gather User Input
-#Your program should ask the user to input the following:
-#A person's name (make sure the name is formatted in title case).
-#A month, day, and year (for a specific date).
-#At least 5 nouns, 5 verbs, 3 adjectives, and 2 adverbs.
-#Replace Placeholders
-#Use the words provided by the user to replace the placeholders in your story template. This should be done using f-strings to format the output.
-#Display the Final Story
-#After gathering all the inputs, display their name, followed by a new line, the date (mm/dd/yy), followed by a new line, then the completed story to the user with the words they provided
-
-
 def story_template():
     print(f"\n{name}\n{month} {day}, {year}")
-    print(f"One {adjective1} day {noun1} was {verb1} and then {noun2} they went to the {noun3}. \nThe {noun3} was {adjective2}, {noun1} was {adverb1} that {noun4} would join them at the {noun3} and play {verb2}")
+    print(f"One {adjective1} day {noun1} was {verb1} and then {noun2} and {noun1} went to the {noun3}.\nThe {noun3} was {adjective2}, {noun1} was {adverb1} that {noun2} would join them at the {noun3} and {verb2} to play a game.\n{noun2} {verb3} {adverb2} to go to {noun4} and {verb4}? {noun1} {verb5} want to leave the {noun3} so he was {adjective3}.\nLater {noun1} went {noun5} and {verb4} because {noun1} was tired. ")
 
 name = input("Enter a person's name: ").title()
 month = input("Enter a month (Ex: November): ").title()
